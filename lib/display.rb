3d177584857c1c733a8b85f53a7fd81132517b7d
neizawa <meeteori@gmail.com>
2022-10-15 14:23:30 +0500

    Add Hangman gameplay

--- lib/display.rb
+++
@@ -1,5 +0,0 @@
-module Display
-  def display_letters
-    puts guessed_letters.join(' ')
-  end
-end
--- lib/game.rb
+++ lib/game.rb
@@ -1,24 +1,38 @@
-require_relative 'display.rb'
-
 DICTIONARY = File.read("google-10000-english-no-swears.txt").split
 
 class Game
-  include Display
-  attr_accessor :secret_word, :guessed_letters, :used_letters
+  attr_accessor :secret_word, :guessed_letters, :used_letters, :letters, :guess, :max_guess
 
   def initialize
     @secret_word = select_random_word
     @letters = secret_word.split('')
     @guessed_letters = Array.new(secret_word.length, '_')
-    @used_letters = ['a']
-    @max_turn = 7
-    @turn = 0
+    @used_letters = []
+    @max_guess = secret_word.length + 5
+    @guess = 0
   end
 
   def play
-    display_letters
-    print "\nType a letter: "
-    input_letter
+    until guess == max_guess || guessed_letters.none?('_')
+      puts "\n#{guessed_letters.join(' ')}"
+      puts "\nYou have #{max_guess - guess} guesses left."
+      puts "Used letters: #{used_letters.join(', ').upcase}"
+      print "Type a letter: "
+      check_letters(input_letter)
+      @guess += 1
+    end
+
+    if guessed_letters.any?('_')
+      puts "\nYou are out of guesses. You lost."
+    else
+      puts "\nCongratulations! You won!"
+    end
+
+    puts "The secret word was '#{secret_word}'."
+  end
+
+  def select_random_word
+    DICTIONARY.select { |word| word.length.between?(5, 12) }.sample
   end
 
   def input_letter
@@ -31,12 +45,14 @@ class Game
       used_letters << letter
       letter
     else
-      print 'Not a letter. Try again: '
+      print 'Wrong input. Try again: '
       input_letter
     end
   end
 
-  def select_random_word
-    DICTIONARY.select { |word| word.length.between?(5, 12) }.sample
+  def check_letters(input)
+    letters.each_with_index do |letter, index|
+      letter == input && guessed_letters[index] = letter
+    end
   end
 end
