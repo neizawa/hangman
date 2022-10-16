351ebc7b3163cbfae93825581e7c66734487dff5
neizawa <meeteori@gmail.com>
2022-10-16 11:19:52 +0500

    Add save and load methods

--- lib/game.rb
+++ lib/game.rb
@@ -1,28 +1,40 @@
-DICTIONARY = File.read("google-10000-english-no-swears.txt").split
+DICTIONARY = File.read('google-10000-english-no-swears.txt').split
 
 class Game
-  attr_accessor :secret_word, :guessed_letters, :used_letters, :letters, :guess, :max_guess
+  attr_accessor :secret_word, :guessed_letters, :used_letters, :letters, :max_guess, :guess
 
-  def initialize
-    @secret_word = select_random_word
+  def initialize(
+    secret_word = select_random_word,
+    used_letters = [],
+    guessed_letters = Array.new(secret_word.length, '_'),
+    guess = 0
+  )
+    @secret_word = secret_word
+    @used_letters = used_letters
+    @guessed_letters = guessed_letters
+    @guess = guess
+
     @letters = secret_word.split('')
-    @guessed_letters = Array.new(secret_word.length, '_')
-    @used_letters = []
     @max_guess = secret_word.length + 5
-    @guess = 0
   end
 
   def play
+    puts "\nIf you want to exit and save the game, type 'save'."
+
     until guess == max_guess || guessed_letters.none?('_')
-      puts "\n#{guessed_letters.join(' ')}"
-      puts "\nYou have #{max_guess - guess} guesses left."
-      puts "Used letters: #{used_letters.join(', ').upcase}"
-      print "Type a letter: "
+      display_info
       check_letters(input_letter)
       @guess += 1
     end
 
     conclude
+  end
+
+  def display_info
+    puts "\n#{guessed_letters.join(' ')}"
+    puts "\nYou have #{max_guess - guess} guesses left."
+    puts "Used letters: #{used_letters.join(', ').upcase}"
+    print 'Type a letter: '
   end
 
   def select_random_word
@@ -32,7 +44,9 @@ class Game
   def input_letter
     letter = gets.chomp.downcase
 
-    if used_letters.include?(letter)
+    if letter == 'save'
+      save_game
+    elsif used_letters.include?(letter)
       print 'Chosen letter is already used. Try another letter: '
       input_letter
     elsif letter.match?(/^[a-z]{1}$/)
@@ -58,5 +72,32 @@ class Game
     end
 
     puts "The secret word was '#{secret_word}'."
+  end
+
+  def save_game
+    file_count = Dir.glob('saves/*.sv').length
+    filename = "#{(file_count + 1).to_s.rjust(3, '0')}.sv"
+
+    File.open("saves/#{filename}", 'w') do |file|
+      JSON.dump({
+                  secret_word: @secret_word,
+                  guessed_letters: @guessed_letters,
+                  guess: @guess,
+                  used_letters: @used_letters
+                }, file)
+    end
+
+    puts "Game saved as '#{filename}'. Thank you for playing!"
+    exit
+  end
+
+  def self.load_game(filename)
+    data = JSON.parse(File.read(filename))
+    Game.new(
+      data['secret_word'],
+      data['used_letters'],
+      data['guessed_letters'],
+      data['guess']
+    ).play
   end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,32 @@
+require 'json'
 require_relative 'lib/game'
 
+def play_game
+  print "Do you want to continue last game? Type 'y' to continue: "
+
+  if gets.chomp.downcase == 'y'
+    print "\nType the name of the save file from 'saves' folder: "
+    load_game
+  else
+    start_game
+  end
+end
+
 def start_game
-  game = Game.new
-  game.play
+  Game.new.play
+  restart_game
+end
+
+def load_game
+  filename = "saves/#{gets.chomp}.sv"
+
+  if File.file?(filename)
+    Game.load_game(filename)
+  else
+    print 'No such file. Try again: '
+    load_game
+  end
+
   restart_game
 end
 
@@ -11,4 +35,4 @@ def restart_game
   gets.chomp.downcase == 'y' ? start_game : puts('Thank you for playing!')
 end
 
-start_game
+play_game
